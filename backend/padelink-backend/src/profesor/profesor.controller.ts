90544b61e347fb979dad20c6c3634bef6e01b3d1
santinoco <santinot002@gmail.com>
2026-02-11 17:06:40 -0300

    Se desarrolla service y controller para la interaccion con la base de datos

--- backend/padelink-backend/src/profesor/profesor.controller.ts
+++ backend/padelink-backend/src/profesor/profesor.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { ProfesorService } from './profesor.service';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { CreateProfesorDto } from './dto/create-profesor.dto';
+
 
 @Controller('profesor')
-export class ProfesorController {}
+export class ProfesorController {
+
+    constructor(private readonly profesorService: ProfesorService){}
+
+    @Get()
+    findall() {
+        return this.profesorService.findAll();
+    }
+
+    @Post()
+    create(@Body() createProfesorDto: CreateProfesorDto) {
+        return this.profesorService.create(createProfesorDto);
+    }
+
+}
--- backend/padelink-backend/src/profesor/profesor.service.ts
+++ backend/padelink-backend/src/profesor/profesor.service.ts
@@ -1,6 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { CreateProfesorDto } from './dto/create-profesor.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Profesor } from './entities/profesor.entity';
 
 @Injectable()
 export class ProfesorService {
-    create 
+    constructor(
+        @InjectRepository(Profesor)
+    private readonly profesorRepository: Repository<Profesor>,
+     ) {}
+    
+    async findAll() {
+        return this.profesorRepository.find();
+    }
+
+    async create(createProfesorDto: CreateProfesorDto) {
+        const profesor = await this.profesorRepository.create(createProfesorDto);
+        return this.profesorRepository.save(profesor);
+    }
+    
+    
+
 }
