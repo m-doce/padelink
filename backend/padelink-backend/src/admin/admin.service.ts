2115203e80048b40624409fbac5404cde098b871
santinoco <santinot002@gmail.com>
2026-02-18 14:43:54 -0300

    Service y controller Club y admin

--- backend/padelink-backend/src/admin/admin.service.ts
+++ backend/padelink-backend/src/admin/admin.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UpdateAdminDto } from './dto/update-admin.dto';
-import { Admin, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
+import { Admin } from './entities/admin.entity';
 
 @Injectable()
 export class AdminService {
--- backend/padelink-backend/src/club/club.service.ts
+++ backend/padelink-backend/src/club/club.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Club } from './entities/club.entity';
+import { CreateClubDto } from './dto/create-club.dto';
+import { UpdateClubDto } from './dto/update-club.dto';
 @Injectable()
-export class ClubService {}
+export class ClubService {
+    constructor(
+        @InjectRepository(Club)
+        private readonly clubRepository: Repository<Club>,
+    ) {}
+
+    async findAll(): Promise<Club[]> {
+        return this.clubRepository.find();
+    }
+
+    async findOne(id: number): Promise<Club | null> {
+        return this.clubRepository.findOne({ where: { club_id: id } });
+    }
+
+    async update(id: number, updateClubDto: UpdateClubDto): Promise<Club | null> {
+        await this.clubRepository.update(id, updateClubDto);
+        return this.clubRepository.findOne({where: {club_id: id}});
+    }
+
+    async create(createClubDto: CreateClubDto): Promise <Club> {
+        const club = this.clubRepository.create(createClubDto);
+        return this.clubRepository.save(club);
+    }
+}
