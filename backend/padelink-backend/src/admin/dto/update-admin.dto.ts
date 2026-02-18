14d661946e70a1721b1488e613ba975e72979f9c
santinoco <santinot002@gmail.com>
2026-02-18 14:29:01 -0300

    Correcciones

--- backend/padelink-backend/src/admin/dto/update-admin.dto.ts
+++ backend/padelink-backend/src/admin/dto/update-admin.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateAdminDto } from './create-admin.dto';
 
-export class UpdateAdminDto extends PartialType(CreateAdminDto) {}
+export class UpdateAdminDto {
+    username: string;
+    password: string;
+}
